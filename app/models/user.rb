21a1348ff5a47a108d21826619b1b1f1c7937d7a
Kathryn Williams <ksw4321@gmail.com>
2018-04-02 16:51:30 +0100

    remember token completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,27 @@
 class User < ApplicationRecord
-
+	
+	before_create :create_remember_token
 	before_save { email.downcase! }
+	
 	validates :name, presence: true
   validates :email, presence: true
   has_secure_password
+  
+  private
+  
+   # Create a remember token
+  def User.new_token
+  	SecureRandom.urlsafe_base64
+  end
+  
+  # Encrypt the token
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+  
+	# Save the encrypted token
+  def create_remember_token
+  	self.remember_digest = User.digest(User.new_token)
+  end
   
 end
